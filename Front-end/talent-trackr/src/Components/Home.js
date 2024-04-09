20c3e8839b5899e44085ff5673b6baf834e230d6
MichelAD1 <michel.a.abidaoud@gmail.com>
2024-04-09 21:05:17 +0300

    Starting home page design

--- Front-end/talent-trackr/src/Components/Home.js
+++ Front-end/talent-trackr/src/Components/Home.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div className="home-container">
+      <div className="left-side">
+        <div className="left-column">
+          <h1>Empowering Connections: Where Careers Align</h1>
+          <p>
+            Whether you're looking to fill key positions within your
+            organization or searching for the perfect career opportunity, we’ve
+            got you covered.
+          </p>
+          <button>Contact Us</button>
+        </div>
+      </div>
+      <div className="right-side">
+        {/* Add content for the right side here */}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
