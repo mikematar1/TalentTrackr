d2be979c2f02776f1564945223cce46bcc2f7690
MichelAD1 <michel.a.abidaoud@gmail.com>
2024-04-09 18:04:23 +0300

    Finishing animations for navbar

--- Front-end/talent-trackr/src/App.js
+++ Front-end/talent-trackr/src/App.js
@@ -12,7 +12,7 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/companies" element={<Companies />} />
           <Route path="/about" element={<About />} />
         </Routes>
