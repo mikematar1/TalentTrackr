55b6fc0ca6ba21786ebe1dbb012ad7d8cef9f923
MichelAD1 <michel.a.abidaoud@gmail.com>
2024-04-10 18:37:04 +0300

    Creating right side of home

--- Front-end/talent-trackr/src/Components/Home.js
+++ Front-end/talent-trackr/src/Components/Home.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
+  const [loaded, setLoaded] = useState(false);
+
+  // Simulate loading delay
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoaded(true);
+    }, 200);
+    return () => clearTimeout(timer);
+  }, []);
+
+  // Apply overflow: hidden to body during transition
+  useEffect(() => {
+    if (!loaded) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "visible";
+    }
+  }, [loaded]);
+
   return (
-    <div className="home-container">
+    <div className={`home-container ${loaded ? "loaded" : ""}`}>
       <div className="left-side">
         <div className="left-column">
           <h1>Empowering Connections: Where Careers Align</h1>
@@ -14,8 +33,25 @@ const Home = () => {
           <button>Contact Us</button>
         </div>
       </div>
+
       <div className="right-side">
-        {/* Add content for the right side here */}
+        <div className="big-square">
+          <div className="right-text">
+            <h1>Fueling Futures: Where Career Paths Converge</h1>
+            <p>
+              Join us in navigating the intersection of talent and opportunity,
+              where dreams find their launchpad and careers take flight.
+            </p>
+          </div>
+        </div>
+        <div className="small-square">
+          <div className="image-square">
+            <img src="/work.jpeg" alt="logo" className="home-image" />
+          </div>
+          <div className="black-square">
+            <h1>Crafting Creativity: Where Ideas Take Shape</h1>
+          </div>
+        </div>
       </div>
     </div>
   );
