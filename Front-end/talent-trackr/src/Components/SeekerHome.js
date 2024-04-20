6a3fd0e94af8e2be559e4a2a4db93cc83552ee46
MichelAD1 <michel.a.abidaoud@gmail.com>
2024-04-20 16:00:07 +0300

    Creating component for job listing

--- Front-end/talent-trackr/src/Components/SeekerHome.js
+++ Front-end/talent-trackr/src/Components/SeekerHome.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { IoLocationOutline } from "react-icons/io5";
-
+import JobListing from "../Global/JobListing";
 const SeekerHome = () => {
   const [loaded, setLoaded] = useState(false);
-  const [percentage] = useState(100); // Default percentage value
 
   // Simulate loading delay
   useEffect(() => {
@@ -23,13 +21,6 @@ const SeekerHome = () => {
     }
   }, [loaded]);
 
-  // Calculate color gradient based on percentage
-  const getColor = () => {
-    const lightness = 20 + (percentage / 100) * 40; // Adjust lightness between 30 and 70
-    const hue = (percentage / 100) * 120; // Hue ranges from 0 to 120 (red to green)
-    return `hsl(${hue}, 100%, ${lightness}%)`; // Use HSL color model for smoother gradient
-  };
-
   return (
     <div className={`home-container seeker ${loaded ? "loaded" : ""}`}>
       <div className="seeker-page">
@@ -38,45 +29,7 @@ const SeekerHome = () => {
           <button>Filter</button>
         </div>
         <div className="job-listings-container">
-          <div className="job-listing">
-            <div className="job-card">
-              <p className="job-title">Junior Graphic Designer</p>
-              <div className="type-salary">
-                <div className="type-box">
-                  <p>INTERNSHIP</p>
-                </div>
-                <div className="salary">
-                  <p>Salary: $20,000 - $25,000</p>
-                </div>
-                <div
-                  className="circle"
-                  id="circle"
-                  style={{ borderColor: getColor() }}
-                >
-                  <p id="percentage" style={{ color: getColor() }}>
-                    {percentage}%
-                  </p>
-                </div>
-              </div>
-              <div className="company-loc">
-                <img src="/job-icon.png" alt="logo1" />
-                <div className="name-loc">
-                  <p className="job-title company">EA Sports</p>
-                  <div className="salary company">
-                    <IoLocationOutline
-                      style={{
-                        color: "#767f8c",
-                        fontSize: "15px",
-                        paddingLeft: "4%",
-                        paddingBottom: "5%",
-                      }}
-                    />
-                    <p>Beirut, Lebanon</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <JobListing />
         </div>
       </div>
     </div>
---
+++ Front-end/talent-trackr/src/Global/JobListing.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+
+import { IoLocationOutline } from "react-icons/io5";
+
+const JobListing = () => {
+  const [percentage] = useState(100); // Default percentage value
+
+  const getColor = () => {
+    const lightness = 20 + (percentage / 100) * 40; // Adjust lightness between 30 and 70
+    const hue = (percentage / 100) * 120; // Hue ranges from 0 to 120 (red to green)
+    return `hsl(${hue}, 100%, ${lightness}%)`; // Use HSL color model for smoother gradient
+  };
+
+  return (
+    <div className="job-listing">
+      <div className="job-card">
+        <p className="job-title">Junior Graphic Designer</p>
+        <div className="type-salary">
+          <div className="type-box">
+            <p>INTERNSHIP</p>
+          </div>
+          <div className="salary">
+            <p>Salary: $20,000 - $25,000</p>
+          </div>
+          <div
+            className="circle"
+            id="circle"
+            style={{ borderColor: getColor() }}
+          >
+            <p id="percentage" style={{ color: getColor() }}>
+              {percentage}%
+            </p>
+          </div>
+        </div>
+        <div className="company-loc">
+          <img src="/job-icon.png" alt="logo1" />
+          <div className="name-loc">
+            <p className="job-title company">EA Sports</p>
+            <div className="salary company">
+              <IoLocationOutline
+                style={{
+                  color: "#767f8c",
+                  fontSize: "15px",
+                  paddingLeft: "4%",
+                  paddingBottom: "5%",
+                }}
+              />
+              <p>Beirut, Lebanon</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default JobListing;
