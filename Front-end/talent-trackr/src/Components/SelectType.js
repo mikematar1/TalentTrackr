b6dea3cab6bb925cc6df0b2d204b52ab894370b4
MichelAD1 <michel.a.abidaoud@gmail.com>
2024-04-30 20:02:07 +0300

    Making changes to select type page

--- Front-end/talent-trackr/src/Components/SelectType.js
+++ Front-end/talent-trackr/src/Components/SelectType.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const SelectType = () => {
   const [loaded, setLoaded] = useState(false);
+  const location = useLocation();
+  let navigate = useNavigate();
+
+  const { data } = location.state || {}; // Destructure the data object from the location's state
 
   // Simulate loading delay
   useEffect(() => {
@@ -21,6 +25,18 @@ const SelectType = () => {
     }
   }, [loaded]);
 
+  const handleSeeker = () => {
+    navigate("/signupseeker", {
+      state: { data },
+    });
+  };
+
+  const handleRecruiter = () => {
+    navigate("/signuprecruiter", {
+      state: { data },
+    });
+  };
+
   return (
     <div className={`signup-container ${loaded ? "loaded" : ""}`}>
       <div className="signup main">
@@ -30,12 +46,12 @@ const SelectType = () => {
             <h1>Who are you?</h1>
           </div>
           <div className="signup-box-btns">
-            <Link to="/signupseeker">
-              <button className="select-btn">Job Seeker</button>
-            </Link>
-            <Link to="/signuprecruiter">
-              <button className="select-btn">Job Recruiter</button>
-            </Link>
+            <button onClick={handleSeeker} className="select-btn">
+              Job Seeker
+            </button>
+            <button onClick={handleRecruiter} className="select-btn">
+              Job Recruiter
+            </button>
           </div>
         </div>
       </div>
