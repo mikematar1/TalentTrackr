b0a3f5835937e16a9d008ccba2dc679b35e91ff6
MichelAD1 <michel.a.abidaoud@gmail.com>
2024-05-04 21:57:19 +0300

    Fixing api listing_details

--- Apis/app/Http/Controllers/ListingController.php
+++ Apis/app/Http/Controllers/ListingController.php
@@ -11,7 +11,7 @@ class ListingController extends Controller
         $listings = Listing::all();
         foreach($listings as $listing){
             $listing_details=json_decode($listing->job_listing_json_object);
-            $listing->details=$listing_details;
+            $listing->listing_details=$listing_details;
         }
         return $listings;
     }
@@ -19,7 +19,7 @@ class ListingController extends Controller
         $listings = Listing::where("recruiter_id","=",$recruiterid)->get();
         foreach($listings as $listing){
             $listing_details=json_decode($listing->job_listing_json_object);
-            $listing->details=$listing_details;
+            $listing->listing_details=$listing_details;
         }
         return $listings;
     }
