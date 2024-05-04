2cab3ec58f0e03fc4c8127976a4df04c9b82095b
MichelAD1 <michel.a.abidaoud@gmail.com>
2024-05-04 21:51:43 +0300

    Creating listings for company profile

--- Front-end/talent-trackr/src/Components/CompanyProfile.js
+++ Front-end/talent-trackr/src/Components/CompanyProfile.js
@@ -1,10 +1,13 @@
 import { useLocation } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
+import GetListingsCompany from "../api-client/HomeSeeker/GetListingsCompany";
+import JobListing from "../Global/JobListing";
 
 const CompanyProfile = () => {
   const [loading, setLoading] = useState(true);
   const [loaded, setLoaded] = useState(false);
+  const [listings, setListings] = useState([]);
 
   const location = useLocation();
   const { job } = location.state || {}; // Destructure data from the previous page
@@ -27,11 +30,49 @@ const CompanyProfile = () => {
     }
   }, [loaded]);
 
+  //API
+  const {
+    status,
+    error,
+    data: listingCompanyData,
+  } = useQuery({
+    queryKey: ["listingCompanyData", id], // Pass the id to the queryKey
+    queryFn: ({ queryKey }) => GetListingsCompany(queryKey[1]), // Extract the id from queryKey and pass it to GetListingsCompany
+    refetchOnWindowFocus: false,
+    staleTime: Infinity,
+    cacheTime: Infinity,
+  });
+
+  useEffect(() => {
+    if (status === "success" && listingCompanyData) {
+      setListings(listingCompanyData);
+      setLoading(false);
+    } else if (error) {
+      console.log(error);
+    }
+  }, [listingCompanyData, status, error]);
+
   return (
     <div className={`company-container ${loaded ? "loaded" : ""}`}>
-      <div className="company-header">
-        <h2>{job.company_details.company_name}</h2>
-      </div>
+      {loading ? (
+        <div className="buffer-space">
+          <div className="buffer-loader"></div>
+        </div>
+      ) : (
+        <>
+          <div className="company-header">
+            <h1>{job.company_details.company_name}</h1>
+            <img src="job-icon.png" alt="logo" />
+          </div>
+          <div className="job-listings-container">
+            {listings.map((job) => (
+              <div key={job.id}>
+                <JobListing key={job.id} job={job} userType={"seeker"} />
+              </div>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 };
