27f2cfc4ee20021d39c3e2789fd274d151afa515
mikematar1 <mike.matar97@gmail.com>
2024-05-04 18:03:46 +0300

    adding schedule

--- Apis/app/Console/Commands/CheckMatches.php
+++ Apis/app/Console/Commands/CheckMatches.php
@@ -37,44 +37,56 @@ class CheckMatches extends Command
             foreach ($listings as $listing) {
 
                 $jobdata = json_decode($listing->job_listing_json_object);
-
-                $data = ["JobData" => $jobdata,"IndexIdsToSearchInto"=>$indeces];
-
-                $url = "https://api.eu.textkernel.com/tx/v10/matcher/joborder";
-
-
-                $curl = curl_init();
-                curl_setopt($curl, CURLOPT_POST, 1);
-                curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
-
-                curl_setopt($curl, CURLOPT_URL, $url);
-                curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-
-                $headers = [
-                "accept: application/json",
-                "content-type: application/json; charset=utf-8",
-                "tx-accountid: 	47510977",
-                "tx-servicekey: cQ8gzhNBmumIqeT2FvT3WQ1SZWXLQMEdt7SGuIa3"
-                ];
-                curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-
-                $result = curl_exec($curl);
-                curl_close($curl);
-                $result=json_decode($result);
-                $result = $result->Value;
-                foreach($result->Matches as $match){
-                    if(ListingMatchesSeeker::where("listing_id","=",$listing->id)->where("seeker_id","=",$match->IndexId)->exists()){
-
-                    }else{
-                        $resume = Resume::find($match->IndexId);
-                        ListingMatchesSeeker::create([
-                            "listing_id"=>$listing->id,
-                            "seeker_id"=>$resume->user_id,
-                            "match_percentage"=>$match->WeightedScore
-                        ]);
-                    }
-
+                $seeker_ids=ListingMatchesSeeker::where("listing_id","=",$listing->id)->select("seeker_id")->get();
+                $resume_ids=[]; //these are the resumes that this listing has already matched with
+                foreach($seeker_ids as $x){
+                    $resumeid = Resume::where("user_id","=",$x->seeker_id)->first();
+                    $resumeid = $resumeid->id;
+                    $resume_ids[] = $resumeid;
                 }
+                $indeces_to_search=array_diff($indeces,$resume_ids);
+                echo(json_encode($indeces_to_search));
+                if(!empty($indeces_to_search)){
+                    $data = ["JobData" => $jobdata,"IndexIdsToSearchInto"=>$indeces_to_search];
+
+                    $url = "https://api.eu.textkernel.com/tx/v10/matcher/joborder";
+
+
+                    $curl = curl_init();
+                    curl_setopt($curl, CURLOPT_POST, 1);
+                    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+
+                    curl_setopt($curl, CURLOPT_URL, $url);
+                    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+                    $headers = [
+                    "accept: application/json",
+                    "content-type: application/json; charset=utf-8",
+                    "tx-accountid: 	47510977",
+                    "tx-servicekey: cQ8gzhNBmumIqeT2FvT3WQ1SZWXLQMEdt7SGuIa3"
+                    ];
+                    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+                    $result = curl_exec($curl);
+                    curl_close($curl);
+                    $result=json_decode($result);
+                    $result = $result->Value;
+                    foreach($result->Matches as $match){
+                        $resume = Resume::find($match->IndexId);
+                        if(ListingMatchesSeeker::where("listing_id","=",$listing->id)->where("seeker_id","=",$resume->user_id)->exists()){
+                            echo("match found");
+                        }else{
+                            $resume = Resume::find($match->IndexId);
+                            ListingMatchesSeeker::create([
+                                "listing_id"=>$listing->id,
+                                "seeker_id"=>$resume->user_id,
+                                "match_percentage"=>$match->WeightedScore
+                            ]);
+                        }
+
+                    }
+                }
+
             }
         } catch (\Exception $e) {
             Log::error('Exception: ' . $e->getMessage());
